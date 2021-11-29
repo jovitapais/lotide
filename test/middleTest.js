331ae32d98eddd9efc00c90184e87bd16f2fe83e
Jovita <jovitaspais@gmail.com>
2021-11-29 23:50:13 +0000

    used mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,12 +2,26 @@ const assertEqual = require('../assertEqual');
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepEqual(middle(['5']), []);
+  });
+  it("returns [4,5] for [1, 2, 3, 4, 5, 6])", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [4,5]);
+  });
+});
+
 
 /*console.log(middle([1,2,2,5,9,10]))
 console.log(middle([1,2,2,5,9]))
 console.log(middle([]))*/
 
-assertArraysEqual(middle([]),[]);
+/*assertArraysEqual(middle([]),[]);
 assertArraysEqual(middle([1]),[]);
 assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[4,5]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[4,5]);*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,26 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
+describe("tail", () => {
+  it('returns 1 for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns ['yoyo'] for ['yoyo', 'yoyo']", () => {
+    assert.deepEqual(tail(['yoyo', 'yoyo']), ['yoyo']); 
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+});
 
 //Test code:
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-(tail(words)); // no need to capture the return value since we are not checking it
-(assertEqual(words.length, 3)); // original array should still have 3 elements!
-
+/*
 const wordsOne = ["Yo Yo"];
 tail(wordsOne);
 assertEqual(wordsOne.length, 1);
 
 const words2 = [];
 let words3 = tail(words2);
-assertEqual(words3.length, 0);
+assertEqual(words3.length, 0);*/
